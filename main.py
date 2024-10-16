d8e813e689b560ec37174a267b65094d06ed0baf
BgmiPaidDdos <gosaiashish052@gmail.com>
2024-10-16 10:01:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ def install_requirements():
 install_requirements()
 
 # 🎛️ Telegram API token (replace with your actual token)
-TOKEN = '7424479555:AAGtNAbqXax_b3b--QnGYi5_XwBbQNw'
+TOKEN = '7420872213:AAGA8T086-XtGWcic6ilzqNWOGxS77VS0_k'
 bot = telebot.TeleBot(TOKEN, threaded=False)
 
 # 🛡️ List of authorized user IDs (replace with actual IDs)
-AUTHORIZED_USERS = [5588464519]
+AUTHORIZED_USERS = [6477461098]
 
 # 🌐 Global dictionary to keep track of user attacks
 user_attacks = {}
